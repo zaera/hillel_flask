af2c13bd066c13ff896588aa466ae841cb76979a
Alex <zaera.alex@gmail.com>
2021-05-11 16:03:24 +0300

    main empty

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from flask import Flask, request, Response
-from utils import average
 
 app = Flask('Homework_punishman')
-
-@app.route('/mean/')
-def mean():
-    return average('hw.csv')
 
 if __name__ == '__main__':
     app.run(port='5000', debug=True)
