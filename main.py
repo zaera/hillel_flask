dfe90ffb7235f3883e03e9f82abd6dcc75bf1c36
Alex <zaera.alex@gmail.com>
2021-05-11 20:05:09 +0300

    hw3 init

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 from flask import Flask, request, Response
-from utils import users
+from utils import average
 
 app = Flask('Homework_punishman')
 
-@app.route('/generate-users/')
-def generate_users():
-    query_params = request.args
-    number = query_params.get('number') or ''
-    default_users_number = 100
-    minimum_users_number = 1
-    maximum_users_number = 100
-
-    if number.isdigit():
-        number = int(number)
-        if number > maximum_users_number or number < minimum_users_number:
-            number = default_users_number
-    else:
-        number = default_users_number
-
-    return users(number)
+@app.route('/mean/')
+def mean():
+    return average('hw.csv')
 
 if __name__ == '__main__':
     app.run(port='5000', debug=True)
--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
-from faker import Faker
+import csv
 
-def users(number: int = 100) -> str:
-    fake = Faker()
-    user_list=''
-    for _ in range(number):
-        user_list = user_list + fake.first_name() + ' ' + fake.free_email() + '<br>'
+def average(file: str) -> str:
+    try:
+        with open(file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            heights = []
+            weights = []
+            for row in reader:
+                heights.append(float(row[reader.fieldnames[1]]))
+                weights.append(float(row[reader.fieldnames[2]]))
+        avg_height = sum(heights) / len(heights)
+        print(avg_height)
+        avg_weights = sum(weights) / len(weights)
+        print(avg_weights)
 
-    return user_list
+        return 'Average height~: ' + str(round(avg_height*2.54, 2)) + ' cm' + '<br>' + 'Average weight~: ' + str(round(avg_weights/2.2046)) + ' kg' + '<br>' + 'Collected: ' + str(len(heights)) + ' items'
+    except Exception:
+
+        return 'OOOOps!! Check filename!'
