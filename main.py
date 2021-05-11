0dda5ad8d82d11530c50cf7951598da8917135a7
Alex <zaera.alex@gmail.com>
2021-05-11 16:19:06 +0300

    hw4 fixes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, Response
+from utils import space
 
 app = Flask('Homework_punishman')
+
+@app.route('/space/')
+def orbital():
+    return space('http://api.open-notify.org/astros.json')
 
 if __name__ == '__main__':
     app.run(port='5000', debug=True)
--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
 from faker import Faker
 import csv
 import requests
+
+def space(link: str) -> str:
+    try:
+        nasa = requests.get(link).json()
+        data = 'There are <font size="+4">' + str(len(nasa['people'])) + "</font> astronauts in space right now. They are: <br>"
+        for name in nasa['people']:
+            data = data + name['name'] + '<br>'
+        return data
+    except Exception:
+        return 'Something wrong with the Nasa link! Sorry.'
