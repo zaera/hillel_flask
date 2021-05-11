0ad8dc31e5a9c0cef6b43ce0cf847f20dd894669
Alex <zaera.alex@gmail.com>
2021-05-11 15:56:37 +0300

    hw2 create

--- utils.py
+++ utils.py
@@ -2,17 +2,11 @@ from faker import Faker
 import csv
 import requests
 
-def file_read(file: str) -> dict():
-    try:
-        with open(file, 'r') as f:
-            file_contents = f.readlines()
-        f.close()
-        file_contents = map(lambda line: line.replace('\n', '<br>'), file_contents)
-        res_dct = []
-        for file_content in file_contents:
-            res_dct.append(file_content)
 
-        return res_dct
-    except Exception:
+def users(number: int = 100) -> str:
+    fake = Faker()
+    user_list=''
+    for _ in range(number):
+        user_list = user_list + fake.first_name() + ' ' + fake.free_email() + '<br>'
 
-        return 'OOOOps!! Check filename!'
+    return user_list
